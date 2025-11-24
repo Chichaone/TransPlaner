6b11301e084b3f5677f79dcbc8b621143f0d3706
Chichaone <112899154+Chichaone@users.noreply.github.com>
2025-11-24 20:34:04 +0600

    Clarify collection route method label

--- src/methods/index.js
+++ src/methods/index.js
@@ -3,7 +3,9 @@ import { method2 } from './method2.js';
 import { method3 } from './method3.js';
 import { method4 } from './method4.js';
 import { method5 } from './method5.js';
+import { method6 } from './method6.js';
+import { method7 } from './method7.js';
 
-const methods = [method1, method2, method3, method4, method5];
+const methods = [method1, method2, method3, method4, method5, method6, method7];
 
 export { methods };
